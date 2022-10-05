af38da3f77d11ee3cab4abe7bb3ab43326ecf67d
Chris Masters <ccm22_@outlook.com>
2022-10-05 17:23:13 +1100

    commit tree 93c717487a06b9f2512b10350dce0cb2d30fddde

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+// Needed to use toUpperCase function to modify the variable name. 
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// If do not preface a variable with const, let or var then it will automatically become a global variable. Not recommended.  
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+// Since bestCustomer was declared as a global variable inside the previous function, just need to reassign the value to 'not bob' inside the new function. 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "Tom";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Greg';
+}
